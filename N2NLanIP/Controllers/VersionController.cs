a9fb85de37d4786441289fee7588f4765d1e9f54
liuhuaxiang <liuhuaxiang@huaqin.com>
2020-06-28 16:26:02 +0800

    更新服务器版本设定

--- N2NLanIP/Controllers/VersionController.cs
+++ N2NLanIP/Controllers/VersionController.cs
@@ -19,5 +19,25 @@ namespace N2NLanIP.Controllers
 
             return Version;
         }
+        // GET api/Version/id
+        public string Get(string Version)
+        {
+            Version = Version.Trim().Replace("\"", "").Replace("\r", "").Replace("\n", "").Replace("\t", "");
+            if (!String.IsNullOrWhiteSpace(Version))
+            {
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "Version.txt", Version);
+            }
+            return Version;
+        }
+        // POST api/values
+        public string Post([FromBody]string value)
+        {
+            string Version = value.Trim().Replace("\"", "").Replace("\r", "").Replace("\n", "").Replace("\t", "");
+            if (!String.IsNullOrWhiteSpace(Version))
+            {
+                 File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "Version.txt", Version);
+            }
+            return Version;
+        }
     }
 }
